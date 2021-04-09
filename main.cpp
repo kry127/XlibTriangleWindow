9e1a8e674ed3f0a7964b3f658b066904f9864d80
kry127 <kry127@yandex.ru>
2021-04-09 06:36:08 +0300

    Added triangle

--- main.cpp
+++ main.cpp
@@ -3,22 +3,68 @@
 #include <iostream>
 #include <cstring>
 #include <cerrno>
+#include <exception>
+#include <unistd.h>
 
 extern int errno;
 
-struct {
-
+typedef struct {
+    bool buttonPressed, wasCreated;
+    int triangleX, triangleY;
+    XColor foregroundColor;
 } SimpleGuiState;
 
 const int InitialWindowWidth = 640;
 const int InitialWindowHeight = 480;
 
+struct timeval tv;
+int x11_fd;
+fd_set in_fds;
+
+class IOException : std::exception {
+
+};
+
+// dirty hacks
+// https://stackoverflow.com/questions/53396936/xlib-disconnection-callback
+// https://github.com/carlodoro88/lxdm/blob/d33391ae1594dd3a9c36e86bb39d6d5be74054a1/src/xconn.c
+// https://cboard.cprogramming.com/c-programming/177009-xlib-program-does-not-want-change-background-color-post1283079.html
+static int CatchIOErrors(Display *dpy)
+{
+    // закрываем файловый дескриптор
+    close(ConnectionNumber(dpy));
+    // не даём библиотеке завершить приложение с ненулевым кодом возврата
+    throw IOException();
+}
+
+void drawContent(Display *d, Window w, SimpleGuiState& state) {
+    int s = XDefaultScreen(d);
+//    XClearWindow(d, w);
+    XClearArea(d, w, 0, 0, 0, 0, False); // DO NOT SET TRUE BECAUSE EXPOSURE IS FIRED!
+
+    short triangleSize = 30;
+
+    /* Если кнопка мыши была нажата хотя бы раз, рисовать треугольник */
+    if (state.wasCreated) {
+        XSetForeground(d, DefaultGC(d, s), state.foregroundColor.pixel);
+        XPoint x[4];
+        x[0].x = state.triangleX;
+        x[0].y = state.triangleY;
+        x[1].x = state.triangleX - triangleSize;
+        x[1].y = state.triangleY + triangleSize * 2;
+        x[2].x = state.triangleX + triangleSize;
+        x[2].y = state.triangleY + triangleSize * 2;
+        x[3].x = state.triangleX;
+        x[3].y = state.triangleY;
+        XDrawLines(d, w, DefaultGC(d, s), x, 4, CoordModeOrigin);
+        XFillPolygon(d, w, DefaultGC(d, s), x, 4, Convex, CoordModeOrigin);
+    }
+}
 
 int main() {
     Display *d;
     Window w;
     XEvent e;
-    std::string msg = "Hello, World!";
     int s;
 
     /* Соединиться с X сервером, если X сервер на удаленной машине
@@ -30,7 +76,16 @@ int main() {
         exit(1);
     }
 
+    /* Ставим собственный обработчик для ошибок IO (например, закрытия окна)
+     * Это предотвратит exit(1) со стороны библиотеки, когда окно закроется,
+     * И мы сможем выйти с нулевым кодом возврата
+     */
+    XSetIOErrorHandler(CatchIOErrors);
+
+    /* Получаем экран по умолчанию */
     s = XDefaultScreen(d);
+
+    try {
 
     /* Создать фоновый цвет окна */
     char bg_color[] = "#25854B";
@@ -39,34 +94,98 @@ int main() {
     XParseColor(d, DefaultColormap(d, s), bg_color, &color);
     XAllocColor(d, DefaultColormap(d, s), &color);
 
+    /* Создаём состояние GUI */
+    SimpleGuiState state;
+    state.wasCreated = false;
+    state.buttonPressed = false;
+    state.foregroundColor;
+
     /* Создать окно */
     w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, InitialWindowWidth, InitialWindowHeight, 1,
-                            XBlackPixel(d, s), color.pixel);
+                            state.foregroundColor.pixel, color.pixel);
 
 
     /* На какие события будем реагировать */
-    XSelectInput(d, w, ExposureMask | ButtonPressMask | ButtonReleaseMask | MotionNotify);
+    XSelectInput(d, w, ExposureMask | ButtonPressMask | ButtonReleaseMask | PointerMotionMask);
 
     /* Вывести окно на экран */
     XMapWindow(d, w);
+    XFlush(d);
+
+    /* получаем файловый дескриптор дисплея X11 */
+    x11_fd = ConnectionNumber(d);
 
     /* Бесконечный цикл обработки событий */
-    while (true) {
-        std::cout << "Before NextEvent" << std::endl;
-        XNextEvent(d, &e);
-        std::cout << "Event received:" << e.type  << std::endl;
+    bool working = true;
+    while (working) {
+        // Create a File Description Set containing x11_fd
+        FD_ZERO(&in_fds);
+        FD_SET(x11_fd, &in_fds);
 
-        /* Перерисовать окно */
-        if (e.type == Expose) {
-            XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
-            XDrawString(d, w, DefaultGC(d, s), 50, 50, msg.c_str(), msg.length());
+        // Set our timer.  One second sounds good.
+        tv.tv_usec = 300000; // 1 000 000 = 1 second
+        tv.tv_sec = 0;
+
+        // Wait for X Event or a Timer
+        int num_ready_fds = select(x11_fd + 1, &in_fds, nullptr, nullptr, &tv);
+        if (num_ready_fds > 0) {
+            // OK! we have events at file descriptor
+        } else if (num_ready_fds == 0) {
+            // Handle timer here
+//            std::cout << "Timer fired!" << std::endl;
+            state.foregroundColor.red += 2;
+            state.foregroundColor.green += 5;
+            state.foregroundColor.blue += 13;
+            state.foregroundColor.red %= 255;
+            state.foregroundColor.green %= 255;
+            state.foregroundColor.blue %= 255;
+            drawContent(d, w, state);
+            continue;
+        } else {
+            std::cout << "Select error!" << std::endl;
         }
-        if (e.type == ButtonRelease) {
-            XDrawString(d, w, DefaultGC(d, s), 100, 100, msg.c_str(), msg.length());
+
+        // Handle XEvents and flush the input
+        while(XPending(d)) {
+            XNextEvent(d, &e);
+
+            switch (e.type) {
+                case Expose:
+                    /* Перерисовать окно */
+                    drawContent(d, w, state);
+                    break;
+                case MotionNotify:
+                    /* Если нажата кнопка, рисовать треугольник в новом месте */
+                    if (state.buttonPressed) {
+                        state.triangleX = e.xmotion.x;
+                        state.triangleY = e.xmotion.y;
+                    }
+                    drawContent(d, w, state);
+                    break;
+                case ButtonPress:
+                    /* Запоминаем, что кнопка была зажата и рисуем треугольник */
+                    state.wasCreated = true;
+                    state.buttonPressed = true;
+                    state.triangleX = e.xbutton.x;
+                    state.triangleY = e.xbutton.y;
+                    drawContent(d, w, state);
+                    break;
+                case ButtonRelease:
+                    /* Перестаём рисовать треугольник */
+                    state.buttonPressed = false;
+                    break;
+            }
+            /* При нажатии кнопки-выход */
+            if (e.type == KeyPress) {
+                working = false;
+                break;
+            }
         }
-        /* При нажатии кнопки-выход */
-        if (e.type == KeyPress)
-            break;
+
+    }
+    } catch (IOException &iex) {
+        // скорее всего, приложение закрыли по крестику
+        std::cout << "Oh, application was probably closed :)" << std::endl;
     }
 
     /* Закрыть соединение с X сервером */
